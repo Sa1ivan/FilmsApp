047f6cebb450ae3b7377edffadf02a75b4bdfe8f
Deni Khadzhiev <92087244+Sa1ivan@users.noreply.github.com>
2023-07-25 22:25:45 +0300

    Update renderFilms.js

--- scripts/renderFilms.js
+++ scripts/renderFilms.js
@@ -8,27 +8,30 @@ export default class Films {
   };
 
   _renderFilm = ({ nameRu, rating, posterUrl }) => {
-    if (rating != "null") {
-      if (rating >= 7) {
-        this._selector.innerHTML += `<div class="film">
-                <div class="film__image" style = "background-image: url(${posterUrl})"></div>
-                <span class="film__name">${nameRu}</span>
-                <div class="film__rank" style = "border: 3px solid green">${rating}</div>
-              </div>`;
-      }
-      if (rating < 7 && rating >= 4) {
-        this._selector.innerHTML += `<div class="film">
-                <div class="film__image" style = "background-image: url(${posterUrl})"></div>
-                <span class="film__name">${nameRu}</span>
-                <div class="film__rank" style = "border: 3px solid yellow">${rating}</div>
-              </div>`;
-      }
-      if (rating < 4 && rating > 0) {
-        this._selector.innerHTML += `<div class="film">
-                <div class="film__image" style = "background-image: url(${posterUrl})"></div>
-                <span class="film__name">${nameRu}</span>
-                <div class="film__rank" style = "border: 3px solid red">${rating}</div>
-              </div>`;
+    if(nameRu != "null" && nameRu != undefined)
+    {
+      if (rating != "null") {
+        if (rating >= 7) {
+          this._selector.innerHTML += `<div class="film">
+                  <div class="film__image" style = "background-image: url(${posterUrl})"></div>
+                  <span class="film__name">${nameRu}</span>
+                  <div class="film__rank" style = "border: 3px solid green">${rating}</div>
+                </div>`;
+        }
+        if (rating < 7 && rating >= 4) {
+          this._selector.innerHTML += `<div class="film">
+                  <div class="film__image" style = "background-image: url(${posterUrl})"></div>
+                  <span class="film__name">${nameRu}</span>
+                  <div class="film__rank" style = "border: 3px solid yellow">${rating}</div>
+                </div>`;
+        }
+        if (rating < 4 && rating > 0) {
+          this._selector.innerHTML += `<div class="film">
+                  <div class="film__image" style = "background-image: url(${posterUrl})"></div>
+                  <span class="film__name">${nameRu}</span>
+                  <div class="film__rank" style = "border: 3px solid red">${rating}</div>
+                </div>`;
+        }
       }
     }
   };
